da79ae550f1e013508617ae315e2c4d69f10193c
altr0616 <aleksandr.trushchinskii@netcracker.com>
2018-06-26 11:37:49 +0300

    Complete array chapter

---
+++ src/sort/insertion/ArrayIns.java
@@ -0,0 +1,45 @@
+package sort.insertion;
+
+public class ArrayIns {
+    private long[] arr;
+    private int nElems;
+
+    public ArrayIns(int max) {
+        arr = new long[max];
+        nElems = 0;
+    }
+
+    public void insert(long value) {
+        arr[nElems] = value;
+        nElems++;
+    }
+
+    public void insertionSort() {
+        int in, out;
+
+        for (out = 1; out < nElems; out ++){
+            long temp = arr[out];
+            in = out;
+
+            while (in > 0 && arr[in - 1] >= temp){
+                arr[in] = arr[in - 1];
+
+                --in;
+                arr[in] = temp;
+            }
+        }
+    }
+
+    private void swap(int one, int two) {
+        long temp = arr[one];
+        arr[one] = arr[two];
+        arr[two] = temp;
+    }
+
+    public void display() {
+        for (int i = 0; i < nElems; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+}
---
+++ src/sort/selection/ArraySel.java
@@ -0,0 +1,45 @@
+package sort.selection;
+
+public class ArraySel {
+    private long[] arr;
+    private int nElems;
+
+    public ArraySel(int max) {
+        arr = new long[max];
+        nElems = 0;
+    }
+
+    public void insert(long value) {
+        arr[nElems] = value;
+        nElems++;
+    }
+
+    public void selectionSort() {
+        int in, out, min;
+
+        for (out = 0; out < nElems - 1; out++) {
+            min = out;
+
+            for (in = out + 1; in < nElems; in++) {
+                if (arr[in] < arr[min]) {
+                    min = in;
+                }
+            }
+
+            swap(out, min);
+        }
+    }
+
+    private void swap(int one, int two) {
+        long temp = arr[one];
+        arr[one] = arr[two];
+        arr[two] = temp;
+    }
+
+    public void display() {
+        for (int i = 0; i < nElems; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+}
